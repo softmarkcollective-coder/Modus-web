d3c7dce11d93cddd654bf31bf15c9c776298a84f
softmarkcollective-coder <softmarkcollective@gmail.com>
2026-02-28 19:02:20 +0000

    update

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,8 @@
+export default function ResetPasswordPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Reset Password Page Works 🎉</h1>
+      <p>If you can see this, routing works.</p>
+    </div>
+  )
+}
